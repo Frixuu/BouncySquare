b9a54db686c9396e6fa44599b48e060c1c9f4eb7
Frixuu <kontakt@lukasz.xyz>
2021-09-05 00:57:03 +0200

    Automatically move the player in the Y axis

--- Arena.gd
+++ Arena.gd
@@ -1,3 +1,10 @@
 extends Node
 
-export var size: int = 720
+export var width: int = 720
+export var height: int = 240
+
+export var x_max: float = float(width / 2)
+export var x_min: float = -x_max
+
+export var y_max: float = float((height / 2) - (32 / 2))
+export var y_min: float = -y_max
--- Player.gd
+++ Player.gd
@@ -1,24 +1,42 @@
 extends Node2D
 
-export var speed = 400  # How fast the player will move (pixels/sec).
-var screen_size  # Size of the game window.
+export var speed_x: float = 400.0
+export var speed_y: float = 300.0
 
-func _process(delta: float):
-	var viewport_rect = get_viewport_rect()
-	viewport_rect.get_area()
-	screen_size = get_viewport_rect().size
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		position += velocity * delta
+var velocity_y: float = speed_y
+
+func _process(dt: float):
+	var velocity = Vector2(0.0, velocity_y)
+	if Input.is_action_pressed("player_right"):
+		velocity.x += speed_x
+	if Input.is_action_pressed("player_left"):
+		velocity.x -= speed_y
+	position += velocity * dt
+	
+	# If the player is vertically out of bounds, reverse their vertical velocity
+	if position.y > Arena.y_max:
+		var difference = position.y - Arena.y_max
+		if difference > Arena.height:
+			position.y = Arena.y_max
+		else:
+			position.y -= difference
+		if velocity_y > 0:
+			velocity_y *= -1.0
+	elif position.y < Arena.y_min:
+		var difference = Arena.y_min - position.y
+		if difference > Arena.height:
+			position.y = Arena.y_min
+		else:
+			position.y += difference
+		if velocity_y < 0:
+			velocity_y *= -1.0
+		
+	# If the player is horizontally out of bounds, just wrap them
+	if position.x > Arena.x_max:
+		position.x -= Arena.width
+	elif position.x < Arena.x_min:
+		position.x += Arena.width
+	
 
 func on_collider_enter(_rid: int, other: Area2D, _other_idx: int, _this_idx: int):
 	if StateManager.current_state == StateManager.State.PLAYING:
