eac032f2c482691099bde2797de6b44347cda335
Frixuu <kontakt@lukasz.xyz>
2021-09-04 14:36:52 +0200

    Adjust screen aspect ratio

--- Player.gd
+++ Player.gd
@@ -3,10 +3,10 @@ extends Area2D
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
-func _ready():
-	screen_size = get_viewport_rect().size
-
 func _process(delta):
+	var viewport_rect = get_viewport_rect()
+	viewport_rect.get_area()
+	screen_size = get_viewport_rect().size
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
---
+++ ViewportAdjuster.gd
@@ -0,0 +1,14 @@
+extends Node
+
+onready var viewport: Viewport = get_viewport()
+export var target_width: int = 720
+
+func _ready():
+	viewport.connect("size_changed", self, "adjust_size")
+	adjust_size()
+
+func adjust_size():
+	var current_size: Vector2 = OS.window_size
+	var scale_factor: float = float(target_width) / float(current_size.x)
+	var new_size: Vector2 = current_size * scale_factor
+	viewport.set_size_override(true, new_size)
