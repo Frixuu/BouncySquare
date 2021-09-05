15fd41120c57a98fc23dc55ec3ff9e04fe7bbf82
Frixuu <kontakt@lukasz.xyz>
2021-09-06 01:51:42 +0200

    Remove unused script

--- MoveParent.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-export var speed: Vector2 = Vector2(0.0, 0.0)
-
-func _process(dt: float):
-	if StateManager.current_state == StateManager.State.PLAYING:
-		var parent = get_parent()
-		(parent as Node2D).position += speed * dt
