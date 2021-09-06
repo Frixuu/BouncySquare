1edba3ea6c7d03f0a29e437bff3e960d80eb1f1f
Frixuu <kontakt@lukasz.xyz>
2021-09-06 02:15:57 +0200

    Reorganize directory structure

--- Utilities/Tweens/FadeInOnFirstRespawn.gd
+++ Utilities/Tweens/FadeInOnFirstRespawn.gd
@@ -4,9 +4,8 @@ onready var tween: Tween = $Tween as Tween
 
 func _ready():
 	self.modulate = Color.transparent
-	StateManager.connect("respawned", self, "on_respawn")
-	
+	var _err = StateManager.connect("respawned", self, "on_respawn", [], CONNECT_ONESHOT)
+
 func on_respawn():
-	StateManager.disconnect("respawned", self, "on_respawn")
 	tween.interpolate_property(self, "modulate", Color.transparent, Color.white, 0.5)
 	tween.start()
